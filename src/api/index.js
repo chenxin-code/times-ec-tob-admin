b61e9e4b9a47767ed6d7c17d0395f0bc8a1bcfc4
zhangli <254804018@qq.com>
2021-07-14 20:02:34 +0800

    fix:调整环境变量

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
-import * as login from './login';
-import * as portal from './portal';
-import * as distribution from './distribution';
-import * as cmsManagement from './cmsManagement';
-import * as market from './market';
+import * as login from "./login";
+import * as portal from "./portal";
+import * as distribution from "./distribution";
+import * as cmsManagement from "./cmsManagement";
+import * as market from "./market";
 
 // 由一体化携带的token，当天有效- 4/25
-let SmAuthorization = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIxMzcxOTI0OTk3NCIsInNjb3BlIjpbImFsbCJdLCJpZCI6MjI1ODI0NjAwOTg0MDczMDUxOCwiZXhwIjoxNjE5MzU2NjI5LCJhdXRob3JpdGllcyI6WyJidXRsZXJfZGlyZWN0b3IiLCJidXRsZXIiLCJzeXNfYWRtaW4iLCJhbnl0aW1lcyJdLCJqdGkiOiJkNjE1YTUyNC1kZTMzLTRlMmItYjgzMC1lNTk4Y2Y1MDhjMDEiLCJjbGllbnRfaWQiOiJ3ZWJfYiJ9.kOSbNRC-TT2TtcFCq0mcq6tr3nr3lelL0jexrYsKyBZdg0d7Aa_fUfHCKP2eLEWW0cNGBsxTR2ZANSrV-hqJPoG6ybdGs82u-JLGEswoeno1hpHh9jdp1bByKi02XGTpt7k9FUmX4pxsGLpk09hDZqT5zobDATAQlx2jX5IPqb-lqv70bN3emWnxPhag4msolDCoHDdF3P32ZyPUuOkMNbT0dh18V057lad8CQGzP913GcrR2-vku-Obv9puXQ86mX_opybA0iAQ6VOdxipHbduuGZoZFuLWUWuj7lsOeOLIuhxiroyssaKE2cANMSV8EDHcFHV6VZNweu63PF7JUg'
-
+let SmAuthorization =
+  "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIxMzcxOTI0OTk3NCIsInNjb3BlIjpbImFsbCJdLCJpZCI6MjI1ODI0NjAwOTg0MDczMDUxOCwiZXhwIjoxNjE5MzU2NjI5LCJhdXRob3JpdGllcyI6WyJidXRsZXJfZGlyZWN0b3IiLCJidXRsZXIiLCJzeXNfYWRtaW4iLCJhbnl0aW1lcyJdLCJqdGkiOiJkNjE1YTUyNC1kZTMzLTRlMmItYjgzMC1lNTk4Y2Y1MDhjMDEiLCJjbGllbnRfaWQiOiJ3ZWJfYiJ9.kOSbNRC-TT2TtcFCq0mcq6tr3nr3lelL0jexrYsKyBZdg0d7Aa_fUfHCKP2eLEWW0cNGBsxTR2ZANSrV-hqJPoG6ybdGs82u-JLGEswoeno1hpHh9jdp1bByKi02XGTpt7k9FUmX4pxsGLpk09hDZqT5zobDATAQlx2jX5IPqb-lqv70bN3emWnxPhag4msolDCoHDdF3P32ZyPUuOkMNbT0dh18V057lad8CQGzP913GcrR2-vku-Obv9puXQ86mX_opybA0iAQ6VOdxipHbduuGZoZFuLWUWuj7lsOeOLIuhxiroyssaKE2cANMSV8EDHcFHV6VZNweu63PF7JUg";
 
 export default {
   ...login,
@@ -14,5 +14,5 @@ export default {
   ...distribution,
   ...cmsManagement,
   ...market,
-	SmAuthorization
-}
+  SmAuthorization,
+};
--- src/api/login.js
+++ src/api/login.js
@@ -1,5 +1,5 @@
-import { fetchApi } from '@/utils/ajax'
-import URL from './urlConfig'
+import { fetchApi } from "@/utils/ajax";
+import URL from "./urlConfig";
 
 //登录
 // export function Login(data) {
@@ -7,9 +7,9 @@ import URL from './urlConfig'
 // }
 
 //登录箭头函数
-export const toLogin = data => fetchApi(URL.LOGIN, data, "POST");
+export const toLogin = (data) => fetchApi(URL.LOGIN, data, "POST");
 //退出登录
-export const toLogout = data => fetchApi(URL.LOGOUT, data, 'POST');
+export const toLogout = (data) => fetchApi(URL.LOGOUT, data, "POST");
 //发送验证码
-export const sendCode = data => fetchApi(URL.SEND_CODE, data, 'POST');
-export const getUserInfo = data => fetchApi(URL.USER_INFO, data, 'GET');
+export const sendCode = (data) => fetchApi(URL.SEND_CODE, data, "POST");
+export const getUserInfo = (data) => fetchApi(URL.USER_INFO, data, "GET");
