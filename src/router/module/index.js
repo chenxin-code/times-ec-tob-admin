590f589031692bfb4a7c19f5be13e329c29b67fd
zhangli <254804018@qq.com>
2021-08-05 16:33:10 +0800

    fix:动态路由

--- src/router/module/index.js
+++ src/router/module/index.js
@@ -1,4 +1,56 @@
 export default [
+  //商品品类管理
+  {
+    path: '/category',
+    name: 'category',
+    menuKey: 'category',
+    meta: {
+      menu: '/category',
+      authKeys: [''],
+      bread: [
+        {
+          path: '/category',
+          name: '商品品类管理',
+        },
+      ],
+      // keepAlive: true
+    },
+    component: () => import('@/pages/category/list'),
+  },
+  //商品上下架
+  {
+    path: '/shelve',
+    name: 'shelve',
+    menuKey: 'shelve',
+    meta: {
+      menu: '/shelve',
+      authKeys: [''],
+      bread: [
+        {
+          path: '/shelve',
+          name: '商品上下架',
+        },
+      ],
+      // keepAlive: true
+    },
+    component: () => import('@/pages/shelve/list'),
+  },
+  {
+    path: '/shelve/edit',
+    name: 'shelveEdit',
+    menuKey: 'shelve',
+    meta: {
+      menu: '/shelve',
+      authKeys: [''],
+      bread: [
+        { path: '/shelve', name: '商品上下架' },
+        { path: 'shelve/edit', name: '商品上下架详情' },
+      ],
+      isUseCache: false,
+      keepAlive: false,
+    },
+    component: () => import('@/pages/shelve/edit'),
+  },
   //负数单管理
   {
     path: '/negative',
@@ -183,5 +235,38 @@ export default [
       // keepAlive: true
     },
     component: () => import('@/pages/distribution/market/deliveryOrder'),
+  },
+  {
+    path: '/commodity',
+    name: 'commodity',
+    menuKey: 'commodity',
+    meta: {
+      menu: '/commodity',
+      authKeys: [''],
+      bread: [
+        {
+          path: '/commodity',
+          name: '商品管理',
+        },
+      ],
+      // keepAlive: true
+    },
+    component: () => import('@/pages/commodity/index'),
+  },
+  {
+    path: '/commodity/edit',
+    name: 'commodityEdit',
+    menuKey: 'commodity',
+    meta: {
+      menu: '/commodity',
+      authKeys: [''],
+      bread: [
+        { path: '/commodity', name: '商品管理' },
+        { path: 'commodity/edit', name: '商品管理详情' },
+      ],
+      isUseCache: false,
+      keepAlive: false,
+    },
+    component: () => import('@/pages/commodity/edit'),
   },
 ]
