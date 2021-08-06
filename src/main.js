81bd306db78d43777bd1b9a1e7cc6e69b7bfb7b8
zhangli <254804018@qq.com>
2021-08-06 19:08:15 +0800

    fix:修复大数据渲染问题

--- src/main.js
+++ src/main.js
@@ -13,33 +13,32 @@ import 'ant-design-vue/dist/antd.less'
 import './assets/css/common.less'
 import router from './router'
 import store from './store'
-import Vuex from "vuex"
-
+import Vuex from 'vuex'
 
 Vue.use(VueQuillEditor)
 
-import * as filters from "./utils/filters"; // global filters
+import * as filters from './utils/filters' // global filters
 //注册全局过滤器方法
-Object.keys(filters).forEach((key) => {
-  Vue.filter(key, filters[key]);
-});
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 import Print from 'vue-print-nb'
-Vue.use(Print);
+Vue.use(Print)
 
 //底部的公共返回保存按鈕
 import FormSubmitButton from './components/FormSubmitButton/index.js'
 Vue.use(FormSubmitButton)
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 console.log(window.location)
 // Vue.use(VueCookies);
 
-Vue.config.productionTip = false;
-Vue.use(Antd);
+Vue.config.productionTip = false
+Vue.use(Antd)
 
 
 new Vue({
-	router,
-	store,
-	render: h => h(App),
-}).$mount('#app');
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app')
--- src/pages/category/list.vue
+++ src/pages/category/list.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="neighborhoodLife">
-    <div class="content-main" ref="content_main">
+    <!-- <div class="content-main" ref="content_main">
       <a-row style="padding: 20px;height: 100%;">
         <a-col>
           <a-table
-              :columns="columns"
-              :data-source="categoryData"
-              :loading="tableLoading"
-              row-key="categoryId"
-              :indentSize="35"
-              :pagination="false">
+            :columns="columns"
+            :data-source="categoryData"
+            :loading="tableLoading"
+            row-key="categoryId"
+            :indentSize="35"
+            :pagination="false"
+          >
             <template slot="children" slot-scope="scope">
               <div class="editable-row-operations">
                 <span v-html="childrenParse(scope.children)"></span>
@@ -18,16 +19,22 @@
           </a-table>
         </a-col>
       </a-row>
-    </div>
+    </div> -->
+    <virtualTree :tableColumns="tableColumn" :tableDatas="categoryData">
+    </virtualTree>
   </div>
 </template>
 
 <script>
-import api from "./../../api";
-
+import api from './../../api'
+import virtualTree from '@/components/virtual-tree'
 export default {
   data() {
     return {
+      tableColumn: [
+        { field: 'name', title: '名称', treeNode: true },
+        { field: 'isChildren', title: '是否末级' },
+      ],
       categoryData: [],
       columns: [
         {
@@ -38,52 +45,58 @@ export default {
         {
           title: '是否末级',
           key: 'children',
-          scopedSlots: {customRender: 'children'},
+          scopedSlots: { customRender: 'children' },
           align: 'left',
           width: 800,
           fixed: 'right',
         },
       ],
       tableLoading: false,
-    };
+    }
   },
-  computed: {
-    childrenParse() {
-      return param => {
-        if (!param || param.length === 0) {
-          return '是';
-        } else {
-          return '否';
-        }
+  components: {
+    virtualTree,
+  },
+  computed: {},
+  methods: {
+    childrenParse(param) {
+      if (!param || param.length === 0) {
+        return '是'
+      } else {
+        return '否'
       }
     },
-  },
-  methods: {
     delChild(data) {
       for (let i = 0; i < data.length; i++) {
-        if(data[i].children.length === 0){
-          delete data[i].children;
-        }else{
-          this.delChild(data[i].children);
+        data[i].isChildren = this.childrenParse(data[i].children)
+        if (data[i].children.length === 0) {
+          delete data[i].children
+        } else {
+          this.delChild(data[i].children)
         }
       }
     },
   },
   mounted() {
-    this.tableLoading = true;
-    api.getCategoryTree({}).then(resp => {
-      if (resp.code === 200) {
-        this.categoryData = resp.data;
-        console.log(this.categoryData);
-        this.delChild(this.categoryData);
-      }
-    }).finally(() => {
-      this.tableLoading = false;
-    });
+    this.tableLoading = true
+    api
+      .getCategoryTree({})
+      .then(resp => {
+        if (resp.code === 200) {
+          this.categoryData = resp.data.map(item => {
+            return {
+              ...item,       
+            }
+          })
+          console.log(this.categoryData)
+          this.delChild(this.categoryData)
+        }
+      })
+      .finally(() => {
+        this.tableLoading = false
+      })
   },
-};
+}
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
