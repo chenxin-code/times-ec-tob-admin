7c50a74d59f60d8c8eee1a5248e4286dbf44ea8f
“zhengcx <zhengcx@zhongketaiyue.com>
2021-08-18 17:28:05 +0800

    更换用户原始id

--- src/pages/accountManagement/index.vue
+++ src/pages/accountManagement/index.vue
@@ -423,8 +423,8 @@ export default {
               this.modelForm.name = res.data.empName
               let employeeOrganizationVOs = res.data.employeeOrganizationVOs[0]
               let idData = {
-                originalId: res.data.id,
-                originalEmpId: res.data.userId,
+                originalId: res.data.userId,
+                originalEmpId: res.data.id,
               }
               this.toAssignRole = { ...employeeOrganizationVOs, ...idData }
             }
